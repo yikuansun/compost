87b3fc7668b9a69379f0aab454c5bf2451e85055
Yichen Sun <50383075+Tigersteve123@users.noreply.github.com>
2020-09-27 11:50:48 -0400

    Update

---
+++ api_key.js
@@ -0,0 +1,2 @@
+export const GOOGLE_API_KEY_FIRESTORE = "AIzaSyAxfjkQhRFa6VppX39XGky3TMkI8M87L3E";  // ben's key for firestore
+export const GOOGLE_API_KEY = "AIzaSyA5CcYkZmiNkdhJoCt4G1NwIUgZgpqoarM"; // ben's key for google map api

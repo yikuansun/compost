8b697e64f28c75ce0e8e6d77a7de8bab84d1edc4
Yikuan Sun <yikuansun2015@gmail.com>
2020-10-01 21:12:01 -0400

    Revert "Merge pull request #12 from yikuansun/yikuansun"
    This reverts commit ffd59880225ff0467fe7cbc003b6bfa121ddc786, reversing
    changes made to 5c797181998b2ad7c44dce1b2ff9897c5ca9bf5a.

--- GoCompost/components/Checker/Checker.js
+++ GoCompost/components/Checker/Checker.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Container, Content } from "native-base";
-import { WebView } from "react-native-webview";
 
 //Components
 class Checker extends Component {
@@ -11,14 +10,7 @@ class Checker extends Component {
     return (
       <Container style={{backgroundColor: "lightblue"}}>
         <Content>
-          <WebView
-            javaScriptEnabled
-            source={{
-              html: `
-              <iframe src="https://compostsearchbar.netlify.app" style="border: none; width: 100%; height: 100%;"></iframe>
-              `,
-            }}
-          />
+          <iframe src="https://compostsearchbar.netlify.app" style="border: none; width: 100%; height: 100%;"></iframe>
         </Content>
       </Container>
     );

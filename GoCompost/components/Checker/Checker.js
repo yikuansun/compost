23400e1c7a75c5fe270379e533530b3428c830e0
Yikuan Sun <yikuansun2015@gmail.com>
2020-10-01 21:18:31 -0400

    added react native webview

--- GoCompost/components/Checker/Checker.js
+++ GoCompost/components/Checker/Checker.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Container, Content } from "native-base";
+import { WebView } from "react-native-webview";
 
 //Components
 class Checker extends Component {
@@ -10,7 +11,14 @@ class Checker extends Component {
     return (
       <Container style={{backgroundColor: "lightblue"}}>
         <Content>
-          <iframe src="https://compostsearchbar.netlify.app" style="border: none; width: 100%; height: 100%;"></iframe>
+          <WebView
+            javaScriptEnabled
+            source={{
+              html: `
+              <iframe src="https://compostsearchbar.netlify.app" style="border: none; width: 100%; height: 100%;"></iframe>
+              `,
+            }}
+          />
         </Content>
       </Container>
     );

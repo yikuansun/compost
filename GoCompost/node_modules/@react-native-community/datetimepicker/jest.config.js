537432bc9c6e23dc2a6df7565aebdb89508b856a
Yichen Sun <50383075+Tigersteve123@users.noreply.github.com>
2020-10-25 12:24:27 -0400

    Update main directory

--- GoCompost/node_modules/@react-native-community/datetimepicker/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  modulePathIgnorePatterns: ['<rootDir>/example/node_modules'],
-  transform: {
-    '^.+\\.js$': require.resolve('react-native/jest/preprocessor.js'),
-  },
-  setupFiles: ['./test/setup.js'],
-};

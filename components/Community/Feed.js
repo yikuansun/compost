6af6628e543decca8738bc7d51359f2e26c306a0
Yichen Sun <50383075+Tigersteve123@users.noreply.github.com>
2020-12-13 16:18:13 -0500

    Update community page feed images

--- components/Community/Feed.js
+++ components/Community/Feed.js
@@ -3,42 +3,16 @@ import { Container, Content } from "native-base";
 import { Text, ScrollView, SafeAreaView, View, FlatList, StyleSheet, Image, Dimensions } from 'react-native';
 
 const imageWidth = Dimensions.get('window').width;
-const headerURL = 'https://tigersteve123.github.io/TigersteveTech/hosted_content/gocompost/assets/Community-banner.PNG';
-
 
 const Community = () => {
-  const [postsDB, setDB] = useState([]);
-  
-  useEffect(() => {
-    fetch('https://tigersteve123.github.io/TigersteveTech/hosted_content/gocompost/posts_db.json')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        setDB(responseJson);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-  const PostView = (props) => {
-    return (
-      <View style={{flexDirection: 'row', flex: 1,}}>
-        <View>
-          <Image source={props.imgLink} style={styles.imgPost} />
-        </View>
-        <View style={{flexShrink: 1,}}>
-          <Text style={{padding: 10,}}>{props.text}</Text>
-        </View>
-      </View>
-  )};
 
   return (
     <View>
       <View>
-        <Image source={{uri:headerURL}} style={styles.imgColumn} />
+        <Image source={require('./Community-banner.png')} style={styles.imgColumn} />
       </View>
       <View style={{padding: 10,}}>
-        <Image source={{uri:'https://raw.githubusercontent.com/Tigersteve123/TigersteveTech/master/hosted_content/gocompost/assets/Dan+statement.PNG'}} style={{width: imageWidth*.9, height: imageWidth*.9*.5625, alignSelf:'center',}} />
+        <Image source={{uri:'https://raw.githubusercontent.com/Tigersteve123/TigersteveTech/master/hosted_content/gocompost/assets/Community-chccs%20post.PNG'}} style={{width: imageWidth*.9, height: imageWidth*.9*.5625, alignSelf:'center',}} />
       </View>
     </View>
     
